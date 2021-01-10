3fb3242faee57d65866778a0ecc1701b22d5a99d
Gardening_Tool <54986171+GardeningTool@users.noreply.github.com>
2021-01-10 04:14:25 -0600

    Removed extraneous things, fixed bad code

--- Renamer/src/main/java/dev/gardeningtool/renamer/Renamer.java
+++ Renamer/src/main/java/dev/gardeningtool/renamer/Renamer.java
@@ -11,14 +11,13 @@ public class Renamer {
     private static final List<File> fileList = new ArrayList<>();
 
     public static void main(String[] args) {
-        PrintStream printStream = System.out;
         File directory = new File("lunar" + File.separator);
         if (!directory.exists()) {
-            printStream.println("Unable to find a directory to rename files from! Exiting...");
+            System.out.println("Unable to find a directory to rename files from! Exiting...");
             directory.mkdir();
             System.exit(-1);
         }
-        getFiles(Objects.requireNonNull(directory.listFiles()));
+        getFiles(directory.listFiles());
         fileList.stream().filter(file -> file.getName().endsWith(".lclass")).forEach(file -> {
             if (!file.renameTo(new File(file.getPath().replace(file.getName(), "") + file.getName().replace(".lclass", ".class")))) {
                 System.err.println("Failed renaming for " + file.getName());
@@ -29,7 +28,7 @@ public class Renamer {
     private static void getFiles(File[] files) {
         for (File file : files) {
             if (file.isDirectory()) {
-                getFiles(Objects.requireNonNull(file.listFiles()));
+                getFiles(file.listFiles());
             } else {
                 fileList.add(file);
                 System.out.println("Found file " + file.getName());
