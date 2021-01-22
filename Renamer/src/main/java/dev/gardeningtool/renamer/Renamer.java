7c9d769ff811a29c649d39df8d6849a4a03deb60
Gardening_Tool <54986171+GardeningTool@users.noreply.github.com>
2021-01-22 00:45:33 -0600

    Cleaned up code

--- Renamer/src/main/java/dev/gardeningtool/renamer/Renamer.java
+++ Renamer/src/main/java/dev/gardeningtool/renamer/Renamer.java
@@ -1,38 +1,56 @@
 package dev.gardeningtool.renamer;
 
 import java.io.File;
-import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class Renamer {
 
-    private static final List<File> fileList = new ArrayList<>();
+    private final File directory = new File("lunar" + File.separator);
+    private final Set<File> files = new HashSet<>();
 
     public static void main(String[] args) {
-        File directory = new File("lunar" + File.separator);
-        if (!directory.exists()) {
-            System.out.println("Unable to find a directory to rename files from! Exiting...");
-            directory.mkdir();
-            System.exit(-1);
-        }
-        getFiles(directory.listFiles());
-        fileList.stream().filter(file -> file.getName().endsWith(".lclass")).forEach(file -> {
-            if (!file.renameTo(new File(file.getPath().replace(file.getName(), "") + file.getName().replace(".lclass", ".class")))) {
-                System.err.println("Failed renaming for " + file.getName());
-            }
-        });
+        Renamer renamer = new Renamer();
+        renamer.init();
+        renamer.rename();
     }
 
-    private static void getFiles(File[] files) {
+    private void init() {
+        if (!directory.isDirectory()) {
+            directory.mkdir();
+            throw new RuntimeException("Unable to find a directory to rename files from! Exiting...");
+        }
+        getFiles(directory.listFiles());
+    }
+
+    private void getFiles(File[] files) {
         for (File file : files) {
             if (file.isDirectory()) {
                 getFiles(file.listFiles());
             } else {
-                fileList.add(file);
-                System.out.println("Found file " + file.getName());
+                if (file.getName().endsWith(".lclass")) {
+                    this.files.add(file);
+                    System.out.println("Found file " + file.getPath());
+                }
             }
         }
     }
+
+    private void rename() {
+        List<File> files = new ArrayList<>();
+        this.files.forEach(file -> {
+            if (!file.renameTo(new File(file.getPath().replace(file.getName(), "") + file.getName().replace(".lclass", ".class")))) {
+                files.add(file);
+            } else {
+                System.out.println("Successfully renamed " + file.getPath());
+            }
+        });
+        files.forEach(file -> System.out.println("Failed renaming for " + file.getPath()));
+        System.out.println("Successfully renamed " + (this.files.size() - files.size()) + " .lclass files!");
+        System.out.println("Failed renaming for " + files.size() + " .lclass files");
+    }
+
 }
